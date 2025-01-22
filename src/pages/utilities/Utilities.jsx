4fdff3b5056d8b4f8398140d0ff4e777371e7ef2
Daniel <daniel.trejo.espino@gmail.com>
2025-01-22 10:56:18 -0600

    test  check date birth

--- src/pages/utilities/Utilities.jsx
+++ src/pages/utilities/Utilities.jsx
@@ -1,31 +1,43 @@
 import { useState } from "react";
 
+import { styled } from "@mui/material/styles";
+import axios from "axios";
 
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
-import { Typography, Box, TextField, InputAdornment, IconButton } from '@mui/material';
-import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
-import { enqueueSnackbar } from 'notistack'
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
+import {
+  Typography,
+  Box,
+  TextField,
+  InputAdornment,
+  IconButton,
+  Button,
+} from "@mui/material";
+import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
+import { enqueueSnackbar } from "notistack";
+import { Gif } from "@mui/icons-material";
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#F5F6FA',
+  backgroundColor: "#F5F6FA",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
-  minWidth: '200px',
-  ...theme.applyStyles('dark', {
-    backgroundColor: '#3e5266',
-    color: '#EDEFF4'
+  minWidth: "200px",
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#3e5266",
+    color: "#EDEFF4",
   }),
 }));
 
 export default function Utilities() {
   const [dataInput, setDataInput] = useState({
-    inputValue: '',
+    inputValue: "",
     codeInput: "",
-    decodeInput: ""
+    decodeInput: "",
   });
+
+  const [query, setQuery] = useState("2445925");
+  const [birthDate, setbirthDate] = useState("");
 
   const [words, setWords] = useState(0);
   const [cursorPosition, setCursorPosition] = useState(0);
@@ -39,7 +51,6 @@ export default function Utilities() {
     const position = event.target.selectionStart;
     setCursorPosition(position);
   };
-
 
   const handleInputChange = (e) => {
     const value = e.target.value;
@@ -58,155 +69,277 @@ export default function Utilities() {
 
     setDataInput({
       codeInput: encodedValue,
-      decodeInput: decodedValue
+      decodeInput: decodedValue,
     });
-
-  }
-
+  };
 
   const handleCopy = (text) => {
-    navigator.clipboard.writeText(text)
-      .then(() => {        
-        const message = 'Texto copiado'
-        enqueueSnackbar(message, { variant: 'info', anchorOrigin : {
-          vertical: 'top',
-          horizontal: 'right',
-         }
-        }
-        )
-        // Puedes agregar una notificación aquí si deseas
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        const message = "Texto copiado";
+        enqueueSnackbar(message, {
+          variant: "info",
+          anchorOrigin: {
+            vertical: "top",
+            horizontal: "right",
+          },
+        });
       })
-      .catch(err => {
+      .catch((err) => {
         console.error("Error al copiar:", err);
       });
   };
 
-  return <>
+  const handleInputDateB = (e) => {
+    if (!/^\d*$/.test(e.target.value)) {
+      const message = "Solo se aceptan numeros";
+      enqueueSnackbar(message, {
+        variant: "error",
+        anchorOrigin: {
+          vertical: "bottom",
+          horizontal: "center",
+        },
+      });
+      return false;
+    }
 
-    <Box sx={{ width: '100%', flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid xs={6}>
-          <Item>
-            <Typography variant="h5" gutterBottom>
-              Encriptar / desencriptar Base 64
-            </Typography>
+    setQuery(e.target.value);
+  };
 
-            <Grid container spacing={3}>
-              <Grid xs={12}>
-                <Box sx={{ marginBottom: 2 }}>
+  const handleDate = async () => {
+    const url = "https://172.20.1.97:3009/api-serv/testOraQuery";
+    const params = {
+      query: `SELECT FECHANACIMIENTO  FROM HSBC.PERSONAS WHERE U_PERSONA = ${query} `,
+      campaign: "0",
+    };
+    try {
+      const response = await axios.post(url, params, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      // console.log(response.status);
+      if (response.status == 200) {
+        const message = "Consulta generada";
+        enqueueSnackbar(message, {
+          variant: "info",
+          anchorOrigin: {
+            vertical: "top",
+            horizontal: "right",
+          },
+        });
+        setbirthDate(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Box sx={{ width: "100%", flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid xs={6}>
+            <Item>
+              <Typography variant="h5" gutterBottom>
+                Encriptar / desencriptar Base 64
+              </Typography>
+
+              <Grid container spacing={3}>
+                <Grid xs={12}>
+                  <Box sx={{ marginBottom: 2 }}>
+                    <TextField
+                      onChange={handleInputChange}
+                      value={dataInput.inputValue}
+                      fullWidth
+                      label="Escribe o pega algo"
+                      variant="outlined"
+                    />
+                  </Box>
+                </Grid>
+                <Grid xs={6}>
                   <TextField
-                    onChange={handleInputChange}
-                    value={dataInput.inputValue}
+                    value={dataInput.codeInput}
+                    name="codeInput"
+                    label="Encode (Base64)"
                     fullWidth
-                    label="Escribe o pega algo"
                     variant="outlined"
+                    sx={{ marginBottom: 2 }}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={() => handleCopy(dataInput.codeInput)}
+                            sx={{
+                              position: "absolute",
+                              top: 0,
+                              right: 0,
+                              zIndex: 1,
+                              color: "inherit",
+                              size: "small",
+                            }}
+                          >
+                            <ContentCopyRoundedIcon sx={{ fontSize: 15 }} />
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
                   />
-                </Box>
-              </Grid>
-              <Grid xs={6}>
-                <TextField
-                  value={dataInput.codeInput}
-                  name="codeInput"
-                  label="Encode (Base64)"
-                  fullWidth
-                  variant="outlined"
-                  sx={{ marginBottom: 2 }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          onClick={() => handleCopy(dataInput.codeInput)}
-                          sx={{
-                            position: 'absolute',
-                            top: 0,
-                            right: 0,
-                            zIndex: 1,
-                            color: 'inherit',
-                            size: 'small'
-                          }}
-                        >
-                          <ContentCopyRoundedIcon sx={{ fontSize: 15 }} />
-                        </IconButton>
-                      </InputAdornment>
-                    )
-                  }}
-                />
-              </Grid>
-              <Grid xs={6}>
-                <TextField
-                  value={dataInput.decodeInput}
-                  label="Decode (Base64)"
-                  name="decodeInput"
-                  fullWidth
-                  variant="outlined"
-                  sx={{ marginBottom: 2 }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          onClick={() => handleCopy(dataInput.decodeInput)}
-                          sx={{
-                            position: 'absolute',
-                            top: 0,
-                            right: 0,
-                            zIndex: 1,
-                            color: 'inherit',
-                            size: 'small'
-                          }}
-                        >
-                          <ContentCopyRoundedIcon sx={{ fontSize: 15 }} />
-                        </IconButton>
-                      </InputAdornment>
-                    )
-                  }}
-                />
-              </Grid>
-            </Grid>
-          </Item>
-        </Grid>
-
-        <Grid xs={6}>
-          <Item>
-            <Typography variant="h5" gutterBottom>
-              Contar caracteres
-            </Typography>
-            <Grid container spacing={3}>
-              <Grid xs={12}>
-                <Box sx={{ marginBottom: 2 }}>
+                </Grid>
+                <Grid xs={6}>
                   <TextField
-                    onChange={handleWords}
-                    onClick={handleCursorPosition}
-                    onKeyUp={handleCursorPosition}
-                    id="standard-textarea"
-                    label="Texto a contar"
-                    placeholder="Texto"
-                    multiline
+                    value={dataInput.decodeInput}
+                    label="Decode (Base64)"
+                    name="decodeInput"
                     fullWidth
-                    // color="secondary"
-                    focused
                     variant="outlined"
+                    sx={{ marginBottom: 2 }}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={() => handleCopy(dataInput.decodeInput)}
+                            sx={{
+                              position: "absolute",
+                              top: 0,
+                              right: 0,
+                              zIndex: 1,
+                              color: "inherit",
+                              size: "small",
+                            }}
+                          >
+                            <ContentCopyRoundedIcon sx={{ fontSize: 15 }} />
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
                   />
-                </Box>
-
-                <Box sx={{ marginBottom: 2 }}>
-                  <Typography variant="h6" gutterBottom>
-                    Total de palabras: {words}
-                  </Typography>
-                  <Typography variant="h6" gutterBottom>
-                    Posición del cursor: {cursorPosition}
-                  </Typography>                  
-                </Box>
+                </Grid>
               </Grid>
+            </Item>
+          </Grid>
 
-            </Grid>
+          <Grid xs={6}>
+            <Item>
+              <Typography variant="h5" gutterBottom>
+                Contar caracteres
+              </Typography>
+              <Grid container spacing={3}>
+                <Grid xs={12}>
+                  <Box sx={{ marginBottom: 2 }}>
+                    <TextField
+                      onChange={handleWords}
+                      onClick={handleCursorPosition}
+                      onKeyUp={handleCursorPosition}
+                      id="standard-textarea"
+                      label="Texto a contar"
+                      placeholder="Texto"
+                      multiline
+                      fullWidth
+                      // color="secondary"
+                      focused
+                      variant="outlined"
+                    />
+                  </Box>
 
-          </Item>
+                  <Box sx={{ marginBottom: 2 }}>
+                    <Typography variant="h6" gutterBottom>
+                      Total de palabras: {words}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                      Posición del cursor: {cursorPosition}
+                    </Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Item>
+          </Grid>
+
+          {/* ----------------------------------------------------         */}
+          <Grid xs={6}>
+            <Item>
+              <Typography variant="h5" gutterBottom>
+                Consultar Fecha nacimiento
+              </Typography>
+              <Grid container spacing={3}>
+                <Grid xs={4}>
+                  <Box sx={{ marginBottom: 2 }}>
+                    <TextField
+                      onChange={handleInputDateB}
+                      label="SOLICITUD"
+                      placeholder="SOLICITUD"
+                      multiline
+                      fullWidth
+                      focused
+                      variant="outlined"
+                      type="number"
+                    />
+                  </Box>
+                </Grid>{" "}
+                {/* fin grid 6 */}
+                <Grid xs={4}>
+                  <Box sx={{ marginBottom: 2 }}>
+                    <Button
+                      onClick={handleDate}
+                      variant="success"
+                      style={{
+                        background: "#ce93d8",
+                      }}
+                    >
+                      Consultar
+                    </Button>
+                  </Box>
+                </Grid>
+                <Grid xs={4}>
+                  <Box sx={{ marginBottom: 2 }}>
+                    <Box sx={{ marginBottom: 2 }}>
+                      {birthDate && (
+                        <Typography variant="h6" gutterBottom>
+                          {JSON.stringify(
+                            birthDate[0].FECHANACIMIENTO,
+                            null,
+                            2
+                          )}
+                        </Typography>
+                      )}
+                    </Box>
+                  </Box>
+                </Grid>
+              </Grid>{" "}
+              {/* fin grid container */}
+            </Item>
+          </Grid>
+
+          <Grid xs={6}>
+            <Item>
+              <Typography variant="h5" gutterBottom>
+                SMS Impulse
+              </Typography>
+              <Grid container spacing={3}>
+                <Grid xs={4}>Numero</Grid>
+                
+                <Grid xs={4}>
+                  <Box sx={{ marginBottom: 2 }}>
+                    <Button
+                      // onClick={handleDate}
+                      variant="success"
+                      style={{
+                        background: "#ce93d8",
+                      }}
+                    >
+                      Disparar SMS
+                    </Button>
+                  </Box>
+                </Grid>
+                <Grid xs={4}>ss</Grid>
+              </Grid>
+            </Item>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
-
-
-  </>
+      </Box>
+    </>
+  );
 }
 
-
+// http://172.20.1.95/sms_hsbc/index2.php?numero=4428450036&mensaje=Prueba_hsbc1&campana=998
